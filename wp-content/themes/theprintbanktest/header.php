1d93f2e12feafc513d560f7881d58eb412168a2a
David <27741806+DbalionisLoader@users.noreply.github.com>
2026-04-09 14:15:05 +0100

    #DB add asset files, fix empty space in walker.php causing theme laod issue

--- wp-content/themes/theprintbanktest/header.php
+++ wp-content/themes/theprintbanktest/header.php
@@ -8,21 +8,33 @@
 
 
 	  <header class="site-header">
-	    <div class="heading">
-	      <h1>Hello New Theme</h1>
-	    </div>
-	    <div class="header_menu">
-	      <?php
-        wp_nav_menu(
-          [
-            'theme_location' => 'primary',
-            'container'      => false,
-            'menu_class' => 'header_menu',
-            //'walker' => new Walker_Basic_Nav(),
-            'items_wrap' => '%3$s',
-            'fallback_cb'        => false,
-          ]
-        );
-        ?>
+	    <div class="pb-heading-wrap">
+
+	      <div class="pb-header_menu">
+	        <?php
+          wp_nav_menu(
+            [
+              'theme_location' => 'primary',
+              'container'      => false,
+              'menu_class' => 'header_menu',
+              'walker' => new Walker_Basic_Nav(),
+              'items_wrap' => '%3$s',
+              'fallback_cb'        => false,
+            ]
+          );
+          ?>
+	      </div>
+	      <section class="pb-hero-section">
+	        <div class="pb-hero-background">
+	          <img srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Header-Image.jpg, <?php echo get_template_directory_uri(); ?>/assets/images/Header-Image-Mobile.jpg 400w"
+	            sizes="(max-width: 600px) 480px, 800px"
+	            alt="Stack of leaflets of 3 different colours" src="<?php echo get_template_directory_uri(); ?>/assets/images/Header-Image.jpg">
+	        </div>
+	        <div class="pb-hero-container">
+	          <div class="pb-hero-content">
+
+	          </div>
+	        </div>
+	      </section>
 	    </div>
 	  </header>
---
+++ wp-content/themes/theprintbanktest/inc/class-walker-nav.php
@@ -0,0 +1,27 @@
+<?php
+
+//Custom walker class to add tailwind css to nav menu
+class Walker_Basic_Nav extends Walker_Nav_Menu
+{
+
+  //Code for the submenu wrapper (child ul start)
+  function start_lvl(&$output, $depth = 0, $args = null) {}
+
+  //Code for the end of submenu wrapper (child ul end)
+  function end_lvl(&$output, $depth = 0, $args = null) {}
+
+  function start_el(&$output, $item, $depth = 0, $args = null, $id = 0)
+  {
+    //$classes = implode(' ', $item->classes);
+
+
+    $url  = ! empty($item->url) ? $item->url : '';
+    $title = ! empty($item->title) ? $item->title : '';
+
+    $output .= '<a href="' . esc_url($url)  . '">';
+    $output .= esc_html($title);
+    $output .= '</a>';
+  }
+
+  function end_el(&$output, $item, $depth = 0, $args = null) {}
+};
